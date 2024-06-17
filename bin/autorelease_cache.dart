7e8c6ea37b2f3a4f0b45bc3ceb0e35b8ba966ce4
masonfrykman <masonfrykman@proton.me>
2024-06-17 11:05:29 -0500

    Auto-releasing cache

---
+++ bin/autorelease_cache.dart
@@ -0,0 +1,40 @@
+import 'dart:typed_data';
+import 'dart:async';
+import 'dart:io';
+
+class AutoreleasingCache {
+  final Map<String, (Uint8List, Timer?)> _store = {};
+
+  AutoreleasingCache();
+
+  bool store(String path, Uint8List data, {Duration? clearAfter}) {
+    if (_store.containsKey(path)) {
+      return false;
+    }
+
+    _store[path] = (
+      data,
+      clearAfter == null
+          ? null
+          : Timer(clearAfter, () {
+              _store.remove(path);
+            })
+    );
+
+    return true;
+  }
+
+  Future<int> storeFromDisk(String path, {Duration? clearAfter}) async {
+    if (_store.containsKey(path)) {
+      return -1;
+    }
+    if (await File(path).exists()) {
+      return 0;
+    }
+
+    Uint8List data = await File(path).readAsBytes();
+    return store(File(path).absolute.path, data, clearAfter: clearAfter)
+        ? data.length
+        : 0;
+  }
+}
--- bin/rbws.dart
+++ bin/rbws.dart
@@ -1,1 +1,10 @@
-void main(List<String> args) {}
+import 'dart:io';
+
+const versionString = "1.0.0";
+
+void main(List<String> args) {
+  print("RBWS v$versionString");
+  print("This software is licensed under the GNU General Public License v3.0.");
+  print(
+      "If this software was not bundled with a LICENSE file, you can find it online at https://www.gnu.org/licenses/gpl-3.0-standalone.html");
+}
