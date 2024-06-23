a652cb0dcd1628f7ec4b2e32ca0f005ed42068da
masonfrykman <masonfrykman@proton.me>
2024-06-23 17:27:28 -0500

    woohoo!!

--- bin/http_instance.dart
+++ bin/http_instance.dart
@@ -1,13 +1,114 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:mime/mime.dart';
+
+import 'http_request.dart';
+import 'http_response.dart';
+
 import 'autorelease_cache.dart';
 
 class HTTPServerInstance {
+  // *************************
+  // * General Configuration *
+  // *************************
+
+  bool debug = false;
+
   dynamic host;
   int port;
   String? generalServeRoot;
-  Map<String, String?>? staticRoutes;
+  Map<(RBWSMethod, String), FutureOr<RBWSResponse> Function(RBWSRequest)>?
+      staticRoutes;
+  SecurityContext? securityContext;
+  String? referralToSecureServer;
 
-  AutoreleasingCache _storage = AutoreleasingCache();
+  // ****************
+  // * Internal Use *
+  // ****************
+
+  AutoreleasingCache storage = AutoreleasingCache();
+  dynamic _serverSocket;
 
   HTTPServerInstance(this.host, this.port,
-      {this.generalServeRoot, this.staticRoutes});
+      {this.generalServeRoot, this.staticRoutes, this.securityContext});
+
+  void start() async {
+    if (securityContext != null) {
+      _serverSocket = await SecureServerSocket.bind(host, port, securityContext,
+          supportedProtocols: ["http/1.1"]);
+      _serverSocket!.listen((socket) => _secureSocketOnListen(socket));
+      return;
+    }
+    _serverSocket = await ServerSocket.bind(host, port);
+    _serverSocket!.listen((socket) => _insecureSocketOnListen(socket));
+  }
+
+  void _secureSocketOnListen(SecureSocket connection) {
+    connection.setOption(SocketOption.tcpNoDelay, true);
+    connection.listen((data) => _conOnData(data, connection));
+  }
+
+  void _insecureSocketOnListen(Socket connection) {
+    connection.setOption(SocketOption.tcpNoDelay, true);
+    connection.listen((data) => _conOnData(data, connection));
+  }
+
+  void _conOnData(Uint8List data, Socket sender) async {
+    var req = RBWSRequest.from(data);
+    if (req == null) {
+      return;
+    }
+
+    var response = await processRequest(req!);
+    sender.add(response.generate11WithData());
+    await sender.flush(); // Make sure we don't start sending other data.
+    sender.destroy();
+    // I've been having issues where data isn't recieved by browsers until
+    // the socket is destroyed. To be so for real, I have no idea if it's
+    // supposed to be like that, but the existence of Connection: keep-alive
+    // seems to point to the contrary. If someone has the answer, please let
+    // me know!
+  }
+
+  Future<RBWSResponse> processRequest(RBWSRequest request) async {
+    // Upgrade-Insecure-Requests
+    if (securityContext == null &&
+        request.headers["Upgrade-Insecure-Requests"]?.trim() == "1" &&
+        referralToSecureServer != null) {
+      return RBWSResponse(303, headers: {
+        "Vary": "Upgrade-Insecure-Requests",
+        "Location": "$referralToSecureServer${request.path}"
+      });
+    }
+
+    // Check static routes
+    var key = (request.method, request.path);
+    if (staticRoutes != null && staticRoutes!.containsKey(key)) {
+      var getStatic = await staticRoutes![key]!(request);
+      if (debug) {
+        getStatic.headers["x-dbg-route-type"] = "static";
+      }
+      return getStatic;
+    }
+
+    // Dynamically load from storage.
+    if (generalServeRoot == null) {
+      return RBWSResponse(404,
+          data: utf8.encode("404 Not Found"),
+          headers: {"Content-Type": "text/plain"});
+    }
+
+    var loadAttempt = await storage.grab("$generalServeRoot${request.path}",
+        ifNotCachedClearAfter: Duration(days: 1));
+    if (loadAttempt == null) {
+      return RBWSResponse(404,
+          data: utf8.encode("404 Not Found"),
+          headers: {"Content-Type": "text/plain"});
+    }
+    return RBWSResponse(200, data: loadAttempt, headers: {
+      "Content-Type": lookupMimeType(request.path) ?? "application/octet-stream"
+    });
+  }
 }
---
+++ bin/http_request.dart
@@ -0,0 +1,114 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+class RBWSRequest {
+  static int NEWLINE = utf8.encode("\n").first;
+
+  String version;
+  String path;
+  RBWSMethod method;
+
+  Map<String, String> headers;
+  Uint8List? data;
+
+  RBWSRequest(this.method, this.path, this.version,
+      {this.data, required this.headers});
+
+  static RBWSRequest? from(Uint8List data) {
+    // First line: Contains version, path, and method.
+    int firstNewline = data.indexWhere((x) => x == NEWLINE);
+    String firstLine = utf8.decode(data.sublist(0, firstNewline));
+    if (firstLine.split(" ").length != 3) {
+      return null; // Could be a problem with space encoding. whatever pitch it.
+    }
+
+    List<String> tokenizedFL = firstLine.split(" ");
+
+    RBWSMethod method = RBWSMethod.fromString(tokenizedFL.first);
+    if (method == RBWSMethod.unrecognized) {
+      return null;
+    }
+    String path = tokenizedFL[1];
+    String version = tokenizedFL[2].split("/").last;
+
+    // Headers
+    List<Uint8List> headerData = [];
+    int beginIndex = firstNewline + 1;
+    for (int i = firstNewline + 1; i < data.length; i++) {
+      if (data[i] == NEWLINE &&
+          i + 1 != data.length &&
+          data[i + 1] != NEWLINE) {
+        headerData.add(data.sublist(beginIndex, i));
+        beginIndex = i + 1;
+        continue;
+      } else if (data[i] == NEWLINE &&
+          i + 1 != data.length &&
+          data[i + 1] == NEWLINE) {
+        headerData.add(data.sublist(beginIndex, i));
+        break;
+      }
+    }
+
+    List<String> stringifiedHeaders = [];
+    for (Uint8List header in headerData) {
+      stringifiedHeaders.add(utf8.decode(header));
+    }
+
+    Map<String, String> headers = processHeaders(stringifiedHeaders);
+
+    // Data (if exists.)
+
+    Uint8List? reqData;
+    if (headers.containsKey("Content-Length")) {
+      int? len = int.tryParse(headers["Content-Length"]!);
+      if (len != null) {
+        reqData = data.sublist(data.length - len);
+      }
+    }
+
+    return RBWSRequest(method, path, version, headers: headers, data: reqData);
+  }
+
+  static Map<String, String> processHeaders(Iterable<String> list) {
+    Map<String, String> s = {};
+    for (String str in list) {
+      int colon = str.indexOf(":");
+      if (colon == -1 || s.containsKey(str.substring(0, colon))) {
+        continue;
+      }
+      s[str.substring(0, colon)] = str.substring(colon + 2);
+    }
+
+    return s;
+  }
+}
+
+enum RBWSMethod {
+  get,
+  post,
+  put,
+  delete,
+  unrecognized;
+
+  @override
+  String toString() {
+    return super.toString().split(".").last.toUpperCase();
+  }
+
+  static RBWSMethod fromString(String container) {
+    container = container.trim().toLowerCase();
+
+    switch (container) {
+      case "get":
+        return get;
+      case "post":
+        return post;
+      case "put":
+        return put;
+      case "delete":
+        return delete;
+    }
+
+    return unrecognized;
+  }
+}
