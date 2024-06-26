9de99aabb67244ef2972720fe92add9a69472785
masonfrykman <masonfrykman@proton.me>
2024-06-26 12:13:53 -0500

    Reformat & /stop static route

--- example/server.dart
+++ example/server.dart
@@ -1,9 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:rbws/rbws.dart';
 
 void logResponse(RBWSResponse response) {
-  print("${DateTime.now()}: [${response.status}] ${response.toRequest?.path}");  
+  print("${DateTime.now()}: [${response.status}] ${response.toRequest?.path}");
 }
 
 void logResponseSecure(RBWSResponse response) {
@@ -19,18 +20,40 @@ void logResponseInsecure(RBWSResponse response) {
 void main(List<String> args) async {
   print("Example public web server using RBWS.");
 
-  HTTPServerInstance server = HTTPServerInstance("localhost", 80, generalServeRoot: "example/public", onResponse: logResponseInsecure);
+  HTTPServerInstance server = HTTPServerInstance("localhost", 80,
+      generalServeRoot: "example/public", onResponse: logResponseInsecure);
   server.referralToSecureServer = "https://localhost:443";
 
-  HTTPServerInstance secure = HTTPServerInstance("localhost", 443, generalServeRoot: "example/public", onResponse: logResponseSecure, securityContext: SecurityContext.defaultContext);
-  
+  HTTPServerInstance secure = HTTPServerInstance("localhost", 443,
+      generalServeRoot: "example/public",
+      onResponse: logResponseSecure,
+      securityContext: SecurityContext.defaultContext);
+
   // BYOC (bring your own certificates)
   // If you don't already have your own certificate, theres a really good guide to generate one at https://deliciousbrains.com/ssl-certificate-authority-for-local-https-development/
   // When you're defining the SAN, set DNS.1 to localhost or to whatever you changed [host] to.
   secure.securityContext!.useCertificateChain("example/cert/localhost.crt");
   secure.securityContext!.usePrivateKey("example/cert/localhost.key");
 
-  secure.staticRoutes = {(RBWSMethod.get, "/secure"): (req) => RBWSResponse.dataFromString(200, "This is being served from the secure server!", headers: {"Content-Type": "text/plain"})};
+  secure.staticRoutes = {
+    (RBWSMethod.get, "/secure"): (req) => RBWSResponse.dataFromString(
+        200, "This is being served from the secure server!",
+        headers: {"Content-Type": "text/plain"}),
+    (RBWSMethod.post, "/stop"): (req) {
+      if (req.data == null) {
+        return RBWSResponse.dataFromString(403, "403 Forbidden");
+      }
+      if (utf8.decode(req.data!, allowMalformed: true).trim() ==
+          "SuperSecretStopKey123") {
+        secure.stop();
+        server.stop();
+        print(
+            "Server stop request succeeded. The program will stop accepting connections but may not exit.");
+        return RBWSResponse.dataFromString(200, "Server shutting down!");
+      }
+      return RBWSResponse.dataFromString(403, "403 Forbidden");
+    },
+  };
   secure.storage = server.storage;
 
   server.start();
