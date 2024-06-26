2d6ff3d6574efbd14df69727a5bf16e24cc1f795
masonfrykman <masonfrykman@proton.me>
2024-06-26 11:44:54 -0500

    Upload example webroot & info on certificates

--- example/server.dart
+++ example/server.dart
@@ -3,30 +3,34 @@ import 'dart:io';
 import 'package:rbws/rbws.dart';
 
 void logResponse(RBWSResponse response) {
-  print("[Insecure Server]: ${DateTime.now()}: [${response.status}] ${response.toRequest?.path}");  
+  print("${DateTime.now()}: [${response.status}] ${response.toRequest?.path}");  
+}
+
+void logResponseSecure(RBWSResponse response) {
+  stdout.write("[Secure]: ");
+  logResponse(response);
+}
+
+void logResponseInsecure(RBWSResponse response) {
+  stdout.write("[Insecure]: ");
+  logResponse(response);
 }
 
 void main(List<String> args) async {
   print("Example public web server using RBWS.");
 
-  HTTPServerInstance server = HTTPServerInstance("localhost", 80, generalServeRoot: "example/public", onResponse: logResponse);
+  HTTPServerInstance server = HTTPServerInstance("localhost", 80, generalServeRoot: "example/public", onResponse: logResponseInsecure);
   server.referralToSecureServer = "https://localhost:443";
-  
-  server.staticRoutes = {
-    (RBWSMethod.get, "/"): (req) async {
-      var indexData = await server.storage.grab("example/public/index.html");
-      if(indexData == null) {
-        return RBWSResponse.dataFromString(404, "404 Not Found", headers: {"Content-Type": "text/plain"});
-      }
-      return RBWSResponse(200, data: indexData, headers: {"Content-Type": "text/html"});
-    }
-  };
 
-  HTTPServerInstance secure = HTTPServerInstance("localhost", 443, generalServeRoot: "example/public", onResponse: logResponse, securityContext: SecurityContext.defaultContext);
+  HTTPServerInstance secure = HTTPServerInstance("localhost", 443, generalServeRoot: "example/public", onResponse: logResponseSecure, securityContext: SecurityContext.defaultContext);
+  
+  // BYOC (bring your own certificates)
+  // If you don't already have your own certificate, theres a really good guide to generate one at https://deliciousbrains.com/ssl-certificate-authority-for-local-https-development/
+  // When you're defining the SAN, set DNS.1 to localhost or to whatever you changed [host] to.
   secure.securityContext!.useCertificateChain("example/cert/localhost.crt");
   secure.securityContext!.usePrivateKey("example/cert/localhost.key");
-  secure.staticRoutes = server.staticRoutes;
-  secure.staticRoutes![(RBWSMethod.get, "/secure")] = (req) => RBWSResponse.dataFromString(200, "This is being served from the secure server!", headers: {"Content-Type": "text/plain"});
+
+  secure.staticRoutes = {(RBWSMethod.get, "/secure"): (req) => RBWSResponse.dataFromString(200, "This is being served from the secure server!", headers: {"Content-Type": "text/plain"})};
   secure.storage = server.storage;
 
   server.start();
