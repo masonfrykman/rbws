9bbe0520b8094312d42085810ef2b664abb7ab64
masonfrykman <masonfrykman@proton.me>
2024-06-26 01:37:08 -0500

    Immutable requests

--- lib/src/http_helpers/http_request.dart
+++ lib/src/http_helpers/http_request.dart
@@ -1,31 +1,62 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
+/// Container for a processed HTTP request.
 class RBWSRequest {
   // (this is basically a constant. not officially.)
   // ignore: non_constant_identifier_names
-  static int NEWLINE = utf8.encode("\n").first;
+  static final int _NEWLINE = utf8.encode("\n").first;
 
-  String version;
-  String path;
-  RBWSMethod method;
+  /// The HTTP version. Most likely will be "1.1".
+  String get version => _version;
+  late String _version;
+  
 
-  Map<String, String> headers;
-  Uint8List? data;
+  /// The requested path. Always starts with a "/".
+  String get path => _path;
+  late String _path;
+  
 
-  RBWSRequest(this.method, this.path, this.version,
-      {this.data, required this.headers});
+  /// HTTP method used.
+  RBWSMethod get method => _method;
+  late RBWSMethod _method;
+  
 
-  RBWSRequest.dataFromString(this.method, this.path, this.version,
-      {required this.headers, String? data}) {
+  /// HTTP headers included in the request. If the request includes duplicate header keys, it will default to only storing the first.
+  Map<String, String> get headers => _headers;
+  late Map<String, String> _headers;
+  
+
+  /// The request's body.
+  Uint8List? get data => _data;
+  Uint8List? _data;
+  
+  RBWSRequest(RBWSMethod method, String path, String version,
+      {Uint8List? data, Map<String, String>? headers}) {
+        _data = data;
+        _headers = headers ?? {};
+        _method = method;
+        _path = path;
+        _version = version;
+      }
+
+  RBWSRequest.dataFromString(RBWSMethod method, String path, String version,
+      {Map<String, String>? headers, String? data}) {
+    _method = method;
+    _path = path;
+    _version = version;
     if (data != null) {
-      this.data = utf8.encode(data);
+      _data = utf8.encode(data);
     }
+    _headers = headers ?? {};
   }
 
+  /// Parses a request from raw data. 
+  /// 
+  /// If it cannot discern a valid request, it returns null.
   static RBWSRequest? from(Uint8List data) {
     // First line: Contains version, path, and method.
-    int firstNewline = data.indexWhere((x) => x == NEWLINE);
+    int firstNewline = data.indexWhere((x) => x == _NEWLINE);
     String firstLine = utf8.decode(data.sublist(0, firstNewline));
     if (firstLine.split(" ").length != 3) {
       return null; // Could be a problem with space encoding. whatever pitch it.
@@ -44,15 +75,15 @@ class RBWSRequest {
     List<Uint8List> headerData = [];
     int beginIndex = firstNewline + 1;
     for (int i = firstNewline + 1; i < data.length; i++) {
-      if (data[i] == NEWLINE &&
+      if (data[i] == _NEWLINE &&
           i + 1 != data.length &&
-          data[i + 1] != NEWLINE) {
+          data[i + 1] != _NEWLINE) {
         headerData.add(data.sublist(beginIndex, i));
         beginIndex = i + 1;
         continue;
-      } else if (data[i] == NEWLINE &&
+      } else if (data[i] == _NEWLINE &&
           i + 1 != data.length &&
-          data[i + 1] == NEWLINE) {
+          data[i + 1] == _NEWLINE) {
         headerData.add(data.sublist(beginIndex, i));
         break;
       }
@@ -78,6 +109,7 @@ class RBWSRequest {
     return RBWSRequest(method, path, version, headers: headers, data: reqData);
   }
 
+  // Splits HTTP headers using format <key>: <value> into a map.
   static Map<String, String> processHeaders(Iterable<String> list) {
     Map<String, String> s = {};
     for (String str in list) {
@@ -92,6 +124,7 @@ class RBWSRequest {
   }
 }
 
+/// HTTP methods
 enum RBWSMethod {
   get,
   post,
