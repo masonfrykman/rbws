5d1c34e902bdbdf96527675a30d0c96ed112760c
masonfrykman <masonfrykman@proton.me>
2025-12-05 10:09:12 -0600

    oops! forgot to export the new classes

--- lib/rbws.dart
+++ lib/rbws.dart
@@ -3,4 +3,6 @@ export 'src/http_instance.dart' show HTTPServerInstance;
 export 'src/http_helpers/http_request.dart' show RBWSRequest;
 export 'src/http_helpers/http_response.dart' show RBWSResponse;
 export 'src/http_helpers/http_method.dart' show RBWSMethod;
+export 'src/http_helpers/http_status.dart' show HTTPStatusCode;
 export 'src/exceptions/path_dne.dart' show PathDoesNotExistException;
+export 'src/exceptions/server_running.dart' show ServerRunningException;
