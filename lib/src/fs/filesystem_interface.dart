f1a0bf068ad91d05511622fc4d0d1eab6346085c
masonfrykman <masonfrykman@proton.me>
2025-12-18 12:23:54 -0600

    Mixin providing standard functions to load & unload given a path.

---
+++ lib/src/fs/filesystem_interface.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+mixin FilesystemStorable {
+  /// Loads a file from the filesystem given a path, returning the data it contains.
+  FutureOr<Uint8List?> load(String path);
+
+  /// Loads a file from the filesystem given a path.
+  void loadSilently(String path);
+
+  /// Removes any data stored that's associated with a path.
+  void purge(String path);
+
+  /// Removes all data stored.
+  void purgeAll();
+
+  /// Whether there's data being stored associated with a path.
+  bool contains(String path);
+}
