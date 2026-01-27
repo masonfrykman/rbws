4571e8718eaec3005af10c029fc4263b046821a2
masonfrykman <masonfrykman@proton.me>
2026-01-27 11:37:39 -0600

    Fix logic error in Rooted.prefixed()

--- lib/src/util/rooted.dart
+++ lib/src/util/rooted.dart
@@ -6,10 +6,10 @@ mixin Rooted {
   /// Cleans and prefixes a path with the [root]
   String prefixed(String path) {
     path = path.replaceAll("..", ""); // prevent escaping the root.
+    if (path.startsWith(root)) return path; // Already prefixed
     if (!path.startsWith("/")) {
       path = "/$path";
     }
-    if (path.startsWith(root)) return path; // Already prefixed
     return root + path;
   }
 }
